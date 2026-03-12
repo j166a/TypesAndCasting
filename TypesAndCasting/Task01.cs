6d61a5cb7e78614d3a5c116073998ae5f45a99ef
Imad Ahmed <j166a775433@gmail.com>
2026-03-12 11:39:43 +0000

    Add class Task01

---
+++ TypesAndCasting/Task01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndCasting
+{
+    internal class Task01
+    {
+        public static void Run()
+        {
+            float floatVariable = 3.80f;
+            string stringVariable = "h";
+            double doubleVariable = 2.5;
+            Console.WriteLine(floatVariable);
+            Console.WriteLine(stringVariable);
+            Console.WriteLine(doubleVariable);
+            
+        }
+    }
+}
