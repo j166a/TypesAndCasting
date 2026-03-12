9ce9159d3519b15dc6d8cffbe5a886a30ef37681
Yosefgid <jossykasshun@yahoo.com>
2026-03-12 12:36:24 +0000

    Add Commentry

--- TypesAndCasting/Task01.cs
+++ TypesAndCasting/Task01.cs
@@ -16,26 +16,39 @@ namespace TypesAndCasting
             Console.WriteLine(floatVariable);
             Console.WriteLine(stringVariable);
             Console.WriteLine(doubleVariable);
+
+            //Implicit conversion from int to decimal
+
             int myInt = 2;
             decimal ourDecimal = myInt;
+
+            //decimal to double
+
             decimal ourDecimal2 = 1.1m;
             double ourDouble = (double)ourDecimal2;
             Console.WriteLine(ourDouble);
-            //
+
+            //float to int
+
             float ourFloat = 2.9f;
             int ourInt = (int)ourFloat;
             Console.WriteLine(ourInt);
+
             // float to decimal
             decimal ourDecimal3 = (decimal)ourFloat;
             Console.WriteLine(ourDecimal3);
-            // d
+
+            // doubel to flaot 
+            double ourDouble2 = 3.678908765436578;
+            float ourFloat1 = (float)ourDouble2;
+            Console.WriteLine(ourFloat1);
 
 
-            
 
 
 
-            
+
+
         }
     }
 }
