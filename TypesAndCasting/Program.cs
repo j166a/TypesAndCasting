cb6e5f2a531fe1ae3c326b9ae3f251ba0b855958
Imad Ahmed <j166a775433@gmail.com>
2026-03-12 12:56:38 +0000

    Create method MultiplyInt

--- TypesAndCasting/Program.cs
+++ TypesAndCasting/Program.cs
@@ -6,6 +6,15 @@
         {
             Task00.Run();
             Task01.Run();
+
+
+            //Task04
+            double num1 = 2.2;
+            decimal num2 = 4.9m;
+            
+            int result = Task04.MultiplyInt((int)num1, (int)num2);
+            Console.WriteLine(result);
+
         }
     }
 }
---
+++ TypesAndCasting/Task04.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndCasting
+{
+    internal class Task04
+    {
+        public static int MultiplyInt(int A, int B)
+        {
+            return A * B;
+        }
+    }
+}
