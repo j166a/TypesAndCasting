bd7162cf8980490940dc55faa938d2081b971232
Imad Ahmed <j166a775433@gmail.com>
2026-03-12 11:16:56 +0000

    Add class Task00

---
+++ TypesAndCasting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TypesAndCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task00.Run();
+        }
+    }
+}
---
+++ TypesAndCasting/Task00.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndCasting
+{
+    internal class Task00
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This works!");
+        }
+    }
+}
